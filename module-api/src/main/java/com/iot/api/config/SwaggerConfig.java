ed40edc62719269a571a135d1896cea4c09bf179
Grégory <fulgueiras.gregory@gmail.com>
2026-07-15 18:03:13 +0200

    feat: Swagger/OpenAPI configuration

---
+++ module-api/src/main/java/com/iot/api/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.iot.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("IoT Hub API")
+                        .description("API centralisée pour objets connectés")
+                        .version("1.0.0"));
+    }
+}
