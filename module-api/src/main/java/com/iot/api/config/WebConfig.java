b5abf9b1021c353bdd75ce48938cbfea8845f3d8
Grégory <fulgueiras.gregory@gmail.com>
2026-07-20 19:15:18 +0200

    feat: CORS config + WebConfig + CorsProperties

---
+++ module-api/src/main/java/com/iot/api/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.iot.api.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final CorsProperties corsProperties;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(new String[0]))
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
